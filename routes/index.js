27c8318175d7653c015396dc7100a358a3bde94a
Satorki <sebastian.kedzier@gmail.com>
2024-07-12 10:22:45 +0200

    pushing the messages to the index site works

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,31 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: new Date().toLocaleString(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toLocaleString(),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Odin Mini Message Board", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+  const message = req.body.message;
+  const name = req.body.name;
+
+  messages.push({
+    text: message,
+    user: name,
+    added: new Date().toLocaleString(),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
